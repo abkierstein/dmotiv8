be1bac827c5556599606ca37d9267ac0e5080831
Adam Kierstein <adamk@zillowgroup.com>
2019-08-26 09:21:25 -0700

    dmotiv8: moving search into a function

--- dmotiv8.py
+++ dmotiv8.py
@@ -3,14 +3,24 @@
 import twitter
 from auth_lib import auth
 
+
+def t_search(search_criteria):
+    """
+    Search twitter for a given criteria
+
+    Return a single post
+
+    The intended usage of this function is to grab a single post from a user
+    """
+    search = api.GetSearch(term=search_criteria,count=1,return_json=True)
+    output = [post['created_at'] + ":\n" + post['text'] for post in search['statuses']][0]
+    return output
+
+
 auth = auth()
 api = twitter.Api(consumer_key=auth.consumer_key,
                   consumer_secret=auth.consumer_secret,
                   access_token_key=auth.access_token_key,
                   access_token_secret=auth.access_token_secret)
 
-t_search = api.GetSearch(term="@cloud_opinion",
-                         count=1,
-                         return_json=True)
-
-[print(post['created_at'] + ":\n" + post['text']) for post in t_search['statuses']]
+print(t_search("@cloud_opinion"))
