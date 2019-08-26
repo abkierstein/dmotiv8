e4b5738f949f6d300941f531d60ac9b76559b285
Adam Kierstein <adamk@zillowgroup.com>
2019-08-26 09:42:03 -0700

    adding example auth_lib.py

---
+++ auth_lib.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class auth:
+    def __init__(self):
+        self.consumer_key='<your consumer_key here>'
+        self.consumer_secret='<your consumer_secret here>'
+        self.access_token_key='<your access_token_key here>'
+        self.access_token_secret='<your access_token_secret here>'
